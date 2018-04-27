49176e53962716285b42b822d130df40be7c2b2e
Adam Ludian <a.ludian@ideamotive.co>
2018-04-27 11:20:31 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    scope module: :v1 do
+      get '/stats/weekly/', to: 'trips#weekly_stats'
+      get '/stats/monthly/', to: 'trips#monthly_stats'
+      post '/trips', to: 'trips#create'
+    end
+  end
 end
