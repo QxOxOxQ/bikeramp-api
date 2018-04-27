eb90a83de5fd5274b0fae3bb3f1f6a78aa152ef8
Adam Ludian <a.ludian@ideamotive.co>
2018-04-27 11:35:18 +0200

    Add empty trips controller

---
+++ app/controllers/api/v1/trips_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TripsController < ApplicationController
+      def weekly_stats
+      end
+
+      def monthly_stats
+      end
+
+      def create
+      end
+    end
+  end
+end
