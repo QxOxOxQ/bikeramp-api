cbc292289ec3c94fcef74be5a6ff7f3bea7c4550
Adam Ludian <a.ludian@ideamotive.co>
2018-04-28 12:12:11 +0200

    add weekly serializer

---
+++ app/serializers/stats/weekly_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Stats::WeeklySerializer < ActiveModel::Serializer
+  attributes :total_distance, :total_price
+
+  def total_price
+    "#{object.sum("price")}PLN"
+  end
+
+  def total_distance
+    "#{object.sum("distance") / 1000}KM"
+  end
+end
