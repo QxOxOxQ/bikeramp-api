21d531275487694a714b886549ac20d2fd3cc692
Adam Ludian <a.ludian@ideamotive.co>
2018-04-28 12:26:59 +0200

    Add trip request monthly & weekly

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -4,9 +4,22 @@ module Api
   module V1
     class TripsController < ApplicationController
       def weekly_stats
+        trips = Trip.current_week
+        if trips.empty?
+          render json: { error: "TripsDoesNotExistInCurrentWeek" }, status: :not_found
+        else
+          render json: trips, serializer: Stats::WeeklySerializer, status: :ok
+        end
       end
 
+
       def monthly_stats
+        trips = Trip.current_month
+        if trips.empty?
+          render json: { error: "TripsDoesNotExistInCurrentMonth" }, status: :not_found
+        else
+          render json: Stats::Monthly::Fetch.call(trips), status: :ok
+        end
       end
 
       def create
